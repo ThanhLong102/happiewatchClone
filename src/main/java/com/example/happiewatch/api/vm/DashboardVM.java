ad28634605308970432a073ea4a61ade50d7a872
Hieu <Thanglong@gmail.com>
2022-08-31 22:29:00 +0700

    add dashboard

---
+++ src/main/java/com/example/happiewatch/api/vm/DashboardVM.java
@@ -0,0 +1,11 @@
+package com.example.happiewatch.api.vm;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class DashboardVM {
+    private Date startDate;
+
+    private Date endDate;
+}
